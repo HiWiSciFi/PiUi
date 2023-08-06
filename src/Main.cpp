e8096c92719c47e58936dfa1561cdcd02ecdd530
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2023-08-06 14:43:57 +0200

    added glfw

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+#include <glfw/glfw3.h>
+
 int main() {
+	glfwInit();
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Testwindow", nullptr, nullptr);
 	std::cout << "Hello World!" << std::endl;
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 }
