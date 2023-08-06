d939207a67f49140b9b9c7ddb52094a05439d30f
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2023-08-06 16:11:08 +0200

    switched to gles

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
-#include <glad/glad.h>
+#include <EGL/egl.h>
+#include <GLES3/gl3.h>
 #include <GLFW/glfw3.h>
 
 int main() {
 	glfwInit();
 
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Testwindow", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	/*while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 	}*/
-
-	gladLoadGLES2Loader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress));
 
 	const GLubyte* glVersionStr = glGetString(GL_VERSION);
 	std::cout << "GL Version: " << glVersionStr << std::endl;
