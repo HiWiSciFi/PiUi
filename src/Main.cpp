110e40d9c0cf8f2d886a01bc982f3bf498f646ce
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2023-08-06 15:32:54 +0200

    added opengl ES

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 
+#include <glad/glad.h>
 #include <glfw/glfw3.h>
 
 int main() {
 	glfwInit();
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Testwindow", nullptr, nullptr);
-	std::cout << "Hello World!" << std::endl;
+	glfwMakeContextCurrent(window);
 
-	while (!glfwWindowShouldClose(window)) {
+	/*while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
-	}
+	}*/
+
+	gladLoadGLES2Loader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress));
+
+	const GLubyte* glVersionStr = glGetString(GL_VERSION);
+	std::cout << "GL Version: " << glVersionStr << std::endl;
 
 	glfwTerminate();
 }
